1744e671ff812a61ed6787385a8ccd09ca058863
Thanh <103243995+giangngocthanh07@users.noreply.github.com>
2026-05-19 03:20:13 +0000

    [LeetCode Sync] Runtime - 333 ms (95.46%), Memory - 0.0B (100.00%)

--- my-solutions/1258-article-views-i/solution.sql
+++ my-solutions/1258-article-views-i/solution.sql
@@ -1,5 +1,4 @@
 -- Write your PostgreSQL query statement below
-SELECT DISTINCT author_id AS id
-FROM Views
-WHERE author_id = viewer_id
-ORDER BY id ASC
+SELECT DISTINCT v.author_id AS id
+FROM Views AS v
+WHERE v.author_id = v.viewer_id
